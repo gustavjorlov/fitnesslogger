50ebe8a15e0b79e17951f5734e6e6510b7d9e7ec
Gustav Jorlöv <gustav.jorlov@kits.se>
2015-10-24 01:41:50 +0200

    fixed the problem.. maybe

--- server/app.js
+++ server/app.js
@@ -32,7 +32,7 @@ app.get("/code", function(req, res){
             console.log(":(");
             res.send(":(");
         }else{
-            console.log(body);
+            console.log(r);
             res.send(":)");
         }
     });
